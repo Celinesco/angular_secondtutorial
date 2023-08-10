138e0f5ce6ad64ccaa26920a12b9f29e244e6cdd
celines <celina.alsina@gmail.com>
2023-08-10 13:53:19 +0200

    New component to send messages to the user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component'; /* this was added automatically after ng generate component */
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; /* this was added automatically after ng generate component */
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component'; /* this was added automatically after ng generate component */
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
+    MessagesComponent,
   ] /* declarations are added automatically after ng generate component */,
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
