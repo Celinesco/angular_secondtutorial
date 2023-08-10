cd9200c468a4ea3cd62f3bb8bb6e402a08c03e55
celines <celina.alsina@gmail.com>
2023-08-10 11:30:28 +0200

    Added comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component'; /* this was added automatically after ng generate component */
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; /* this was added automatically after ng generate component */
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+  ] /* declarations are added automatically after ng generate component */,
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
