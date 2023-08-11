e4bcd7642b88c13d036deef82ab59fca829b2c2e
celines <celina.alsina@gmail.com>
2023-08-11 12:40:32 +0200

    Added dashboard route, 404notfound and redirect from / to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { paths } from './paths';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
   { path: paths.heroesList, component: HeroesComponent },
+  { path: paths.dashboard, component: DashboardComponent },
+  {
+    path: '',
+    redirectTo: paths.dashboard,
+    pathMatch: 'full',
+  } /**para que redirija automaticamente a dashboard.  */,
+  { path: '**', pathMatch: 'full', component: PagenotfoundComponent },
 ]; /*Supongo que cada ruta, se escribe como un objeto.  tengo que leer sobre rutas anidadas, rutas con segmentos iguales e*/
 
 @NgModule({
