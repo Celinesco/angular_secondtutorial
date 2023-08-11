8567bd1dbb0c0f5b2bf1bc49635d38095d185788
celines <celina.alsina@gmail.com>
2023-08-11 11:42:50 +0200

    Created new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { paths } from './paths';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent },
+  { path: paths.heroesList, component: HeroesComponent },
 ]; /*Supongo que cada ruta, se escribe como un objeto.  tengo que leer sobre rutas anidadas, rutas con segmentos iguales e*/
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component'; /* this was added automatically after ng generate component */
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component'; /* this was added automatically after ng generate component */
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component'; /* this was added automatically after ng generate component */
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { MessagesComponent } from './messages/messages.component'; /* this was a
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
+    DashboardComponent,
   ] /* declarations are added automatically after ng generate component */,
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
